f2d99ac49a27957a3b461d1acf06cbb6ea46f978
fadyshaher <85324687+fadyshaher@users.noreply.github.com>
2021-06-13 22:56:38 +0200

    Add files via upload
    header file of standard types of project

---
+++ Standard_types_ARM.h
@@ -0,0 +1,78 @@
+/*
+ * Standard_types_ARM.h
+ *
+ *  Created on: 26 May 2021
+ *      Author: Administrator
+ */
+
+#ifndef STANDARD_TYPES_ARM_H_
+#define STANDARD_TYPES_ARM_H_
+
+typedef unsigned char         uint8_t;          /*           0 .. 255             */
+typedef signed char           sint8_t;          /*        -128 .. +127            */
+typedef unsigned short        uint16_t;         /*           0 .. 65535           */
+typedef signed short          sint16_t;         /*      -32768 .. +32767          */
+typedef unsigned long         uint32_t;         /*           0 .. 4294967295      */
+typedef signed long           sint32_t;         /* -2147483648 .. +2147483647     */
+typedef unsigned long long    uint64_t;         /*       0..18446744073709551615  */
+typedef signed long long      sint64_t;
+typedef float                 float32_t;
+typedef double                float64_t;
+
+
+
+
+
+#define PA0 0
+#define PA1 1
+#define PA2 2
+#define PA3 3
+#define PA4 4
+#define PA5 5
+#define PA6 6
+#define PA7 7
+
+#define PB0 0
+#define PB1 1
+#define PB2 2
+#define PB3 3
+#define PB4 4
+#define PB5 5
+#define PB6 6
+#define PB7 7
+
+#define PC0 0
+#define PC1 1
+#define PC2 2
+#define PC3 3
+#define PC4 4
+#define PC5 5
+#define PC6 6
+#define PC7 7
+
+
+#define PD0 0
+#define PD1 1
+#define PD2 2
+#define PD3 3
+#define PD4 4
+#define PD5 5
+#define PD6 6
+#define PD7 7
+
+
+#define PE0 0
+#define PE1 1
+#define PE2 2
+#define PE3 3
+#define PE4 4
+#define PE5 5
+
+#define PF0 0
+#define PF1 1
+#define PF2 2
+#define PF3 3
+#define PF4 4
+
+
+#endif /* STANDARD_TYPES_ARM_H_ */

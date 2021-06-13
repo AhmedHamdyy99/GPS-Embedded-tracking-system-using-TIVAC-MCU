1bccc7f51b15ce46fae54d45c3e7d39f06d2974e
FadyIbrahim52 <85261189+FadyIbrahim52@users.noreply.github.com>
2021-06-13 13:47:59 -0700

    Update LCD (3).h
    Header file for LCD

--- LCD (3).h
+++ LCD (3).h
@@ -3,24 +3,24 @@
 
 #include "Standard_types_ARM.h"
 #include "common_macros_ARM.h"
-#include "tm4c123gh6pm.h"
+
 #include "delay.h"
-
-/*******************************************************************************
+#include "tm4c123gh6pm.h"
+/***************************
  *                      Preprocessor Macros                                    *
- *******************************************************************************/
+ ***************************/
 /* LCD HW Pins */
-#define RS PC4
-#define RW PC5
-#define E  PC6
-#define LCD_CTRL_PORT GPIO_PORTC_AHB_DATA_R       
-#define LCD_CTRL_PORT_DIR GPIO_PORTC_AHB_DIR_R
-#define LCD_CTRL_PORT_DEN GPIO_PORTC_AHB_DEN_R
+#define RS 0
+#define RW 1
+#define E  2
+#define LCD_CTRL_PORT GPIO_PORTD_DATA_R       
+#define LCD_CTRL_PORT_DIR GPIO_PORTD_DIR_R
+#define LCD_CTRL_PORT_DEN GPIO_PORTD_DEN_R
 
 
-#define LCD_DATA_PORT GPIO_PORTD_AHB_DATA_R
-#define LCD_DATA_PORT_DIR GPIO_PORTD_AHB_DIR_R
-#define LCD_DATA_PORT_DEN GPIO_PORTD_AHB_DEN_R
+#define LCD_DATA_PORT GPIO_PORTB_DATA_R
+#define LCD_DATA_PORT_DIR GPIO_PORTB_DIR_R
+#define LCD_DATA_PORT_DEN GPIO_PORTB_DEN_R
 
 /* LCD Commands */
 #define CLEAR_COMMAND 0x01
@@ -28,15 +28,19 @@
 #define CURSOR_OFF 0x0C
 #define CURSOR_ON 0x0E
 #define SET_CURSOR_LOCATION 0x80 
-
-/*******************************************************************************
+#define AUTO_INCREMENT 0x06
+#define wake_up 0x30
+#define display_on 0x0F
+/***************************
  *                      Functions Prototypes                                   *
- *******************************************************************************/
+ ***************************/
 void LCD_sendCommand(uint8_t command);
 void LCD_displayCharacter(uint8_t data);
 void LCD_displayString(const char *Str);
 void LCD_init(void);
 void LCD_clearScreen(void);
+void dmilli_break(int n);
+void dmilli(int n);
 
 
 #endif /* LCD_H_ */
